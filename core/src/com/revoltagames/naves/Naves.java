a755ae9dcd6ccf92d0ef32d75ecc0dcfa2df513a
Truji92 <alejandro.trujillo.7@gmail.com>
2014-10-07 18:01:58 +0200

    Modificada clase principal para arrancar juego correctamente

--- core/src/com/revoltagames/naves/Naves.java
+++ core/src/com/revoltagames/naves/Naves.java
@@ -3,31 +3,39 @@ package com.revoltagames.naves;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.revoltagames.managers.GameStateManager;
 
 public class Naves extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 	
 	public static int WIDTH;
 	public static int HEIGTH;
 	
+	private OrthographicCamera camera;
+	
+	private GameStateManager gameStManager;
+	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 		
 		HEIGTH = Gdx.graphics.getHeight();
 		WIDTH = Gdx.graphics.getWidth();
+		
+		camera = new OrthographicCamera(WIDTH, HEIGTH);
+		camera.translate(WIDTH/2, HEIGTH/2);
+		
+		gameStManager = new GameStateManager();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		//PANTALLA EN NEGRO
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+		gameStManager.update(Gdx.graphics.getDeltaTime());
+		gameStManager.draw();
 	}
 }
