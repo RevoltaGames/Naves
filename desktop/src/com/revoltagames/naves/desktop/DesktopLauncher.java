fa01fd1c8363782e6bb4b7d9974d567b0d67a4c9
Cesar Enrique <caenrique93@gmail.com>
2014-10-07 16:53:35 +0200

    arreglado

---
+++ desktop/src/com/revoltagames/naves/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.revoltagames.naves.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.revoltagames.naves.Naves;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Naves(), config);
+	}
+}
