cccda787a9afd44f2007e54438a85811585bd49c
Cesar Enrique <caenrique93@gmail.com>
2014-10-09 20:37:36 +0200

    arreglado algunos fallos en androidLauncher debido a codigo entrometido

--- android/src/com/revoltagames/naves/android/AndroidLauncher.java
+++ android/src/com/revoltagames/naves/android/AndroidLauncher.java
@@ -1,8 +1,5 @@
 package com.revoltagames.naves.android;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -15,18 +12,5 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Naves(), config);
-	}
-	
-	@Override
-	public void onReceive(Context context, Intent intent) {
-	    String action = intent.getAction();
-	    if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-	        int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-	        if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-	            // Wifi P2P is enabled
-	        } else {
-	            // Wi-Fi P2P is not enabled
-	        }
-	    }
 	}
 }
